344bc21262c4746a5d5163d452648729c36313e9
Chris Hunt <chris.hunt@hudl.com>
2019-11-30 14:17:38 +0000

    Config changes

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ from PIL import Image
 import numpy as np
 import logging
 
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 logger = logging.getLogger("headshot_creator")
-logger.setLevel(logging.INFO)
-ch = logging.StreamHandler()
-logger.addHandler(ch)
 
 base_path = path.join(path.dirname(__file__), "input")
 
@@ -30,14 +29,14 @@ def get_faces(pixels):
 	detector = MTCNN()
 	return detector.detect_faces(pixels)
 
-def extract_face(filename, outname, required_size=(500, 500)):
+def extract_face(filename, outname, required_size=(400, 500)):
 	logger.info(f"Looking for faces in {filename}")
 	pixels = pyplot.imread(filename)
 	results = get_faces(pixels)
 	logger.info(f"Found {len(results)} faces")
 	for i, result in enumerate(results):
 		x1, y1, width, height = result['box']
-		x1, y1, x2, y2 = add_padding(x1, y1, height, width, (40, 80), required_size[0] / required_size[1])
+		x1, y1, x2, y2 = add_padding(x1, y1, height, width, (40, 60), required_size[0] / required_size[1])
 		face = pixels[y1:y2, x1:x2]
 		image = Image.fromarray(face)
 		savename = path.join(path.dirname(__file__), f"output/{outname}_{i}.jpg")
