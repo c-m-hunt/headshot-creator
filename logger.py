09aff0e7356eadc51659bd03e54d9398ba021d2a
Chris Hunt <chris.hunt@hudl.com>
2020-02-02 12:02:52 +0000

    Set logging DEBUG in logger module

--- logger.py
+++ logger.py
@@ -1,6 +1,8 @@
 import logging
 
+
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s"
 )
 logger = logging.getLogger("headshot_creator")
+logger.DEBUG = "DEBUG"
